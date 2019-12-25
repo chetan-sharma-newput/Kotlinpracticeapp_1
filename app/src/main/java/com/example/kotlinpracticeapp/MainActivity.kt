22188679ca58156edd884ddd4388c5fb89a363a7
Surendra Patidar <surendra@newput.com>
2019-12-25 18:18:15 +0530

    push

--- app/src/main/java/com/example/kotlinpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpracticeapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.kotlinpracticeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    var et_name;
+    var et_surname;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
